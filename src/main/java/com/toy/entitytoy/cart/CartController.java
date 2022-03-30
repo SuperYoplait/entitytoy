6506e9e12275f8546d2fc85da1421ba7966bf6df
JangDongBin <jdb9515@naver.com>
2022-03-30 14:32:06 +0900

    상품 계산 완료

--- src/main/java/com/toy/entitytoy/cart/CartController.java
+++ src/main/java/com/toy/entitytoy/cart/CartController.java
@@ -1,6 +1,7 @@
 package com.toy.entitytoy.cart;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.toy.entitytoy.cartitem.CartItem;
 import com.toy.entitytoy.cartitem.CartItemRepository;
@@ -17,19 +18,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CartController {
     private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
 
     private final Long id = 3080L;
     @GetMapping("/my-cart")
     public String my_cart(Model model){
         List<CartItem> select_cartId = cartItemRepository.findByCartId(id);
-        
+        Optional<Cart> select_user = cartRepository.findById(id);
         
         if(select_cartId.isEmpty()){
             System.out.println("비었음");
         }else{
-            for(CartItem item : select_cartId)
-                System.out.println("아이템 목록 : "+ item);
-            //System.out.println("\n전체 상품 가격 : " + );
+            for(CartItem item : select_cartId){
+                System.out.println("아이템 목록 : " + item.getItem().getName() + "\t아이템 가격" + item.getItem().getPrice());
+            }
+            System.out.println("\n전체 상품 가격 : " + select_user.get().getSum_price());
         }
         return "my-cart";
     }
--- src/main/java/com/toy/entitytoy/item/ItemController.java
+++ src/main/java/com/toy/entitytoy/item/ItemController.java
@@ -38,39 +38,39 @@ public class ItemController {
     @GetMapping("/item-insert/{itemId}")
     public String item_insert(Model model, @PathVariable("itemId") Long id) {
         Long i_count = 1L;
-        List<CartItem> select_cart = cartItemRepository.findByCartId(userid); //카트 아이템
-        Optional<Item> select_item = itemRepository.findById(id); //아이템
+        List<CartItem> select_cart = cartItemRepository.findByCartId(userid); // 카트 아이템
+        Optional<Item> select_item = itemRepository.findById(id); // 아이템
         Optional<Cart> select_user = cartRepository.findById(userid);
-        Cart user_cart  = select_user.get(); //카트에 담은 합산 값 계산 하기 위함
+        Cart user_cart = select_user.get(); // 카트에 담은 합산 값 계산 하기 위함
         Item insert_item = select_item.get();// 아이템 밀어넣기 위함
         CartItem saveItem = new CartItem();
-        
 
-        if(user_cart.getId() == null || insert_item.getId() == null){
+        if (user_cart.getId() == null || insert_item.getId() == null) { // 유저 아이디 or 상품 아이디가 null이면 에러
             return "item-error";
         }
+        // 상품 정보들을 상품 리스트에 넣기 위한 작업
+        saveItem.setId(null);
+        saveItem.setCnt(i_count);
+        saveItem.setCart(user_cart);
+        saveItem.setItem(insert_item);
 
-        if(saveItem.getId() == null){
-            saveItem.setId(null);
-            saveItem.setCnt(i_count);
-            saveItem.setCart(user_cart);
-            saveItem.setItem(insert_item);
-        }
+        System.out.println("\n\n" + saveItem);
+        cartItemRepository.save(saveItem); //장바구니 상품 리스트에 저장.
+
+        Long sum_price_calc = user_cart.getSum_price();
+        //장바구니에 담으면 현재 상품 가격도 추가해줘야 함
+        sum_price_calc += saveItem.getItem().getPrice() * saveItem.getCnt();
+
+
+        //추후 오버헤드 때문이라도 수정해야함
+        for(CartItem get_cart_item : select_cart)
+            sum_price_calc += select_item.get().getPrice() * get_cart_item.getCnt();
+        System.out.println("물건 합산 금액" + sum_price_calc);
         
+        user_cart.setSum_price(sum_price_calc);
+        System.out.println(user_cart.getSum_price());
 
-        System.out.println("\n\n"+saveItem);
-        cartItemRepository.save(saveItem);
-
-
-        
-        //Cart_Item newCart_item = new Cart_Item(3000L, 1L, insert_item, call_cart);
-        //System.out.println("\n\n"+newCart_item);
-        //Cart_Item save_Cart_Item = cart_itemRepository.save(newCart_item);
-        //System.out.println("\n\n"+save_Cart_Item);
-
-        //System.out.println("\n\n" + cart_itemRepository.save(newCart_item));
-        //System.out.println("\n\n" + cartRepository.findAll());
-
+        cartRepository.save(user_cart); //합계 한 값 다시 저장.
         return "item-insert";
     }
 
